90fb87ec51cd312c7fb5755bdfd2ae47411c3415
MJurayev <jurayevmansurbek@gmail.com>
2021-04-11 11:39:21 +0500

    server updated

--- src/Contexts/ServerProvider.jsx
+++ src/Contexts/ServerProvider.jsx
@@ -4,7 +4,8 @@ import {  createContext, useContext, useState } from "react"
 const Context = createContext()
  function ServerProvider({children}) {
     // const [server, setServer] = useState("http://node-platform.cleverapps.io")
-    const [server, setServer] = useState("http://localhost:8080")
+    // const [server, setServer] = useState("http://localhost:8080")
+    const [server, setServer] = useState("http://3.141.203.196:8080")
     return (
         <Context.Provider value={{server, setServer }}>
             <Context.Consumer>
